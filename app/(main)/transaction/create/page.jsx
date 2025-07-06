3b079e94b93439f77cfd8c3e9b78f1120aad9cde
Bineet Pradhan <bineet1540@gmail.com>
2025-07-06 13:31:56 +0530

    Ready to deploy @v6 and fixing some bug

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,4 +1,40 @@
-"use client";
+// "use client";
+// import { getUserAccounts } from "@/actions/dashboard";
+// import { defaultCategories } from "@/data/categories";
+// import { AddTransactionForm } from "../_components/transaction-form";
+// import { getTransaction } from "@/actions/transaction";
+
+// export default async function AddTransactionPage({ searchParams }) {
+//   const accounts = await getUserAccounts();
+//   const editId = searchParams?.edit;
+
+//   let initialData = null;
+//   if (editId) {
+//     const transaction = await getTransaction(editId);
+//     initialData = transaction;
+//   }
+
+//   return (
+//     <div className="w-full px-5 bg-gray-900 min-h-screen">
+//       <div className="flex justify-center  mb-5 -mb-5">
+//         <br />
+//         <br />
+//         <h1 className="text-5xl gradient-title text-white center">
+//           Add Transaction
+//         </h1>
+//       </div>
+//       <AddTransactionForm
+//         accounts={accounts}
+//         categories={defaultCategories}
+//         editMode={!!editId}
+//         initialData={initialData}
+//       />
+//     </div>
+//   );
+// }
+
+export const dynamic = "force-dynamic";
+
 import { getUserAccounts } from "@/actions/dashboard";
 import { defaultCategories } from "@/data/categories";
 import { AddTransactionForm } from "../_components/transaction-form";
@@ -10,17 +46,14 @@ export default async function AddTransactionPage({ searchParams }) {
 
   let initialData = null;
   if (editId) {
-    const transaction = await getTransaction(editId);
-    initialData = transaction;
+    initialData = await getTransaction(editId);
   }
 
   return (
     <div className="w-full px-5 bg-gray-900 min-h-screen">
-      <div className="flex justify-center  mb-5 -mb-5">
-        <br />
-        <br />
-        <h1 className="text-5xl gradient-title text-white center">
-          Add Transaction
+      <div className="flex justify-center mb-5 -mb-5">
+        <h1 className="text-5xl gradient-title text-white text-center">
+          {editId ? "Edit Transaction" : "Add Transaction"}
         </h1>
       </div>
       <AddTransactionForm
